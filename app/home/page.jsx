c8f880e7e9faa61a36bbc3b032301806f62055de
yrkeviin <kevineziquiellima160@gmail.com>
2025-04-17 14:12:16 -0300

    Feat: Text box and image, resume for page

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -6,15 +6,37 @@ import Button from "../../components/Button"
 
 export default function Home() {
     return (
-        <div className={styles.containerBanner}>
-            <Header />
-            <div className={styles.textBanner}>
-                <img src="/textVemP-removebg-preview.png" alt="" />
+        <div className={styles.container}>
+            <div className={styles.containerBanner}>
+                <Header />
+                <div className={styles.textBanner}>
+                    <img src="/textVemP-removebg-preview.png" alt="" />
+                </div>
+
+                <Button 
+                text="Crie seu post aqui!"
+                />
             </div>
 
-            <Button 
-            text="Crie seu post aqui!"
-            />
+            <div className={styles.containerContent}>
+                <div className={styles.text}>
+                    <h1>Bem vindo ao Party Life! 🪅</h1>
+                    <p>A sua rede social definitiva para festas inesquecíveis!
+                    Aqui, você descobre e compartilha os melhores eventos da sua cidade — sejam baladas, festas na praia, encontros alternativos ou rolês secretos. Publique seu evento, convide a galera e fique por dentro de onde a diversão está rolando.
+                    A festa começa aqui. 🎉🌴</p>
+
+                    <p>🕺 Crie seu perfil, publique festas</p>
+                    <p>🎉 Descubra o que tá rolando perto de você</p>
+                    <p>📍 Use nosso mapa interativo pra achar eventos</p>
+                    <p>📢 Divulgue seu evento pra uma galera que realmente curte festa</p>
+
+                    <p>Seja você um organizador, um DJ ou só alguém em busca da próxima vibe… o PartySpot é o seu ponto de partida.</p>
+                </div>
+
+                <div className={styles.imageText}>
+                    <img src="./imageBeach.jpg" alt="" />
+                </div>
+            </div>
         </div>
     );
 }
