b791526b91536994a64ccedabb36b7373dd05318
yrkeviin <kevineziquiellima160@gmail.com>
2025-05-15 16:28:17 -0300

    Feat: Details pagee

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,16 @@
 import "./globals.css";
 
 export const metadata = {
-	title: "My First NextJS App",
-	};
+	title: "PARTY LIFE",
+};
 
-	export default function RootLayout({ children }) {
-		return (
-			<html>
-				<body>{children}</body>
-			</html>
-		);
+export default function RootLayout({ children }) {
+	return (
+		<html>
+			<head>
+				<link rel="icon" href="/icons/favicon.ico" />
+			</head>
+			<body>{children}</body>
+		</html>
+	);
 }
