9b4390d68ea1b368c5c0fdbc80fe3e25cb9f2077
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-05-20 16:22:16 -0300

    Update Styles of the Sobrenos Page

--- app/sobrenos/page.jsx
+++ app/sobrenos/page.jsx
@@ -13,28 +13,30 @@ export default function Sobrenos() {
                     <h1>Sobre o PARTYLIFE!</h1>
                 </div>
             </div>
-            <Image src="/logopartylife.png" alt="Logo PARTY LIFE" width={450} height={200} className={styles.image} />
+            <div className={styles.imageteam}>
+                <Image src="/bannerPraia.jpg" alt="Logo PARTY LIFE" width={850} height={600}/>
+            </div>
+            
             <div className={styles.content}>
                 <div className={styles.textContent}>
                     <p className={styles.descript}>Bem-vindo ao PARTYLIFE, a rede social que te conecta aos melhores eventos e festas da sua cidade! Nosso objetivo é simplificar a descoberta de rolês, proporcionando uma experiência intuitiva e dinâmica tanto no mobile quanto na web.</p>
                 </div>
 
                 <div className={styles.teamContent}>
-                    <h2>Nossa equipeee!</h2>
+                    <h2 className={styles.teamtitle}>Nossa equipeee!</h2>
                     <div className={styles.teamCards}>
-                        <CardSobreNos nome="Enzo Turcovic" foto="/profileenzo.png" funcao="Desenvolvedor Frontend" />
-                        <CardSobreNos nome="Enzo Turcovic" foto="/profileenzo.png" funcao="Desenvolvedor Frontend" />
-                        <CardSobreNos nome="Enzo Turcovic" foto="/profileenzo.png" funcao="Desenvolvedor Frontend" />
-                        <CardSobreNos nome="Enzo Turcovic" foto="/profileenzo.png" funcao="Desenvolvedor Frontend" />
-                        <CardSobreNos nome="Enzo Turcovic" foto="/profileenzo.png" funcao="Desenvolvedor Frontend" />
-                        <CardSobreNos nome="Enzo Turcovic" foto="/profileenzo.png" funcao="Desenvolvedor Frontend" />
-                        <CardSobreNos nome="Enzo Turcovic" foto="/profileenzo.png" funcao="Desenvolvedor Frontend" />
+                        <CardSobreNos nome="Giovanna Alba" foto="/profileenzo.png" funcao="Product Owner" />
+                        <CardSobreNos nome="Maria Parma" foto="/profileenzo.png" funcao="Scrum Master" />
+                        <CardSobreNos nome="Enzo Turcovic" foto="/profileenzo.png" funcao="Desenvolvedor" />
+                        <CardSobreNos nome="Kevin Lima" foto="/profileenzo.png" funcao="Desenvolvedor" />
+                        <CardSobreNos nome="Luiz Ricci" foto="/profileenzo.png" funcao="Desenvolvedor" />
+                        <CardSobreNos nome="Lucas Zani" foto="/profileenzo.png" funcao="Desenvolvedor" />
                     </div>
                 </div>
 
-                <div className={styles.textjoin}>
-                    <h2>Junte-se a Nós!</h2>
-                    <p>Se você ama festas e quer fazer parte de uma comunidade vibrante, junte-se ao PartyLife hoje mesmo! Estamos ansiosos para ver suas fotos e ouvir suas histórias de festa.</p>
+                <div className={styles.contentjoin}>
+                    <h2 className={styles.titlejoin}>Junte-se a Nós!</h2>
+                    <p className={styles.descriptjoin}>Se você ama festas e quer fazer parte de uma comunidade vibrante, junte-se ao PartyLife hoje mesmo! Estamos ansiosos para ver suas fotos e ouvir suas histórias de festa.</p>
                 </div>
             </div>
             <Footer />
