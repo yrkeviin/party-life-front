8254e5fd5b964e4dc043007ee9c5257a0e163a5c
yrkeviin <kevineziquiellima160@gmail.com>
2025-05-20 11:35:49 -0300

    Feat: Infos in banner

---
+++ components/InfoDetalhes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from '../styles/InfoDetalhes.module.css';
+
+export default function InfoDetalhes({ text, title, local, date, hour }) {
+  return (
+    <div className={styles.infoContainer}>
+        <h3>{text}</h3>
+        <h1>{title}</h1>
+        <p>{local}</p>
+        <p>{date}</p>
+        <p>{hour}</p>
+    </div>
+  );
+}
