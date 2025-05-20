25a85e43280636f931f951d899767237ee12b30f
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-05-20 13:25:41 -0300

    Update subHeader

--- app/sobrenos/page.jsx
+++ app/sobrenos/page.jsx
@@ -1,5 +1,6 @@
 import styles from "./Sobrenos.module.css";
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 import Image from "next/image";
 import CardSobreNos from "../../components/CardSobreNos";
 
@@ -9,13 +10,13 @@ export default function Sobrenos() {
             <Header />
             <div className={styles.subHeader}>
                 <div className={styles.text}>
-                    <h1>Sobre o PartyLife!</h1>
+                    <h1>Sobre o PARTYLIFE!</h1>
                 </div>
             </div>
             <Image src="/logopartylife.png" alt="Logo PARTY LIFE" width={450} height={200} className={styles.image} />
             <div className={styles.content}>
                 <div className={styles.textContent}>
-                    <p className={styles.descript}>Bem-vindo ao PartyLife, a rede social que te conecta aos melhores eventos e festas da sua cidade! Nosso objetivo é simplificar a descoberta de rolês, proporcionando uma experiência intuitiva e dinâmica tanto no mobile quanto na web.</p>
+                    <p className={styles.descript}>Bem-vindo ao PARTYLIFE, a rede social que te conecta aos melhores eventos e festas da sua cidade! Nosso objetivo é simplificar a descoberta de rolês, proporcionando uma experiência intuitiva e dinâmica tanto no mobile quanto na web.</p>
                 </div>
 
                 <div className={styles.teamContent}>
@@ -36,6 +37,7 @@ export default function Sobrenos() {
                     <p>Se você ama festas e quer fazer parte de uma comunidade vibrante, junte-se ao PartyLife hoje mesmo! Estamos ansiosos para ver suas fotos e ouvir suas histórias de festa.</p>
                 </div>
             </div>
+            <Footer />
         </div>
     );
 }
