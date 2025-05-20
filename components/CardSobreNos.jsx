bce699d12d71fb5527a6458efb51aa951bad2220
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-05-20 11:51:36 -0300

    Create Page Sobre Nós

---
+++ components/CardSobreNos.jsx
@@ -0,0 +1,17 @@
+import styles from '../styles/CardSobreNos.module.css';
+
+export default function CardSobreNos({ nome, foto, funcao }) {
+    return (
+        <div className={styles.card}>
+            <div className={styles.nome}>
+                <h2>{nome}</h2>
+            </div>
+            <div className={styles.fotocontainer}>
+                <img src={foto} alt={nome} />
+            </div>
+            <div className={styles.funcao}>
+                <p>{funcao}</p>
+            </div>
+        </div>
+    );
+}
