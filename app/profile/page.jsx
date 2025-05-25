30efcff18b18e4a9a8b9294e1ef2b23d89422bbd
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-05-25 15:52:35 -0300

    Start of Profile Page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,22 +1,33 @@
 import styles from './Profile.module.css';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import Divisor from '@/components/Divisor';
 
 export default function Profile() {
     return (
         <div className={styles.container}>
-            <Header />
-            <div className={styles.profileContainer}>
-                <div className={styles.profilePicture}>
-                    <img src="/profileenzo.png" alt="Profile Picture" />
+            <div className={styles.content}>
+                <Header />
+                <div className={styles.profileinfo}>
+                    <div className={styles.profileimage}>
+                        <img src="/profilePic.png" alt="Profile Picture" className={styles.image}/>
+                    </div>
+                    <div className={styles.profiletext}>
+                        <h1>Enzo Turcovic</h1>
+                        <p>@turcoo</p>
+                    </div>
                 </div>
-                <div className={styles.profileInfo}>
-                    <h1>Nome do Usuário</h1>
-                    <p>@username</p>
-                    <p>Descrição do usuário</p>
+                < Divisor />
+                <div className={styles.posts}>
+                    <h2>Suas Festas!</h2>
+                    <div className={styles.post}>
+                        <p>Post 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                    <div className={styles.post}>
+                        <p>Post 2: Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
                 </div>
             </div>
-            <Footer />
         </div>
     );
 }
