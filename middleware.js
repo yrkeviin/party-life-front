e1c60956d4ef343aca4f26182ff597ba10964afb
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-05-29 09:12:13 -0300

    Delete middleware.js

--- middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export const config = {
-    matcher: "/",
-};
-
-export default function middleware(req) {
-    return NextResponse.redirect(new URL("/login", req.url));
-}
