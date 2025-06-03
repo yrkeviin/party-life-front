2079460c4e41ff3abd565ada22655d3b2cd1f10a
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-06-03 15:00:15 -0300

    Update Responsiveness and Page Sobrenos

--- app/sobrenos/page.jsx
+++ app/sobrenos/page.jsx
@@ -28,9 +28,79 @@ export default function Sobrenos() {
 
     return (
         <div className={styles.container}>
-            <div className={styles.containerHeader}>
-                <Header />
+            <div className={styles.subHeader}>
+                < Header />
+                
             </div>
+            {/* <div className={styles.imageCard}>
+                <Image
+                    src="/images/imageteam.jpg"
+                    alt="Logo PARTY LIFE"
+                    width={750}
+                    height={550}
+                    className={styles.imageTeam}
+                />
+            </div> */}
+
+            <div className={styles.content}>
+                <div className={styles.text}>
+                    <h1>Seja Bem-vindo(a) ao PARTYLIFE!</h1>
+                </div>
+                <div className={styles.textContent}>
+                    <p className={styles.description}>
+                        Bem-vindo ao PARTYLIFE, a rede social que te conecta aos melhores
+                        eventos e festas da sua cidade! Nosso objetivo é simplificar a
+                        descoberta de rolês, proporcionando uma experiência intuitiva e
+                        dinâmica tanto no mobile quanto na web.
+                    </p>
+                </div>
+
+                <div className={styles.teamContent}>
+                    <h2 className={styles.teamTitle}>Conheça a nossa equipe!</h2>
+                    <div className={styles.teamCards}>
+                        <CardSobreNos
+                            nome="Maria Parma"
+                            foto="/images/profilemaria.jpg"
+                            funcao="Scrum Master"
+                        />
+                        <CardSobreNos
+                            nome="Giovanna Alba"
+                            foto="/images/profilegiovanna.jpg"
+                            funcao="Product Owner"
+                        />
+                        <CardSobreNos
+                            nome="Enzo Turcovic"
+                            foto="/images/profileenzo.jpg"
+                            funcao="Desenvolvedor"
+                        />
+                        <CardSobreNos
+                            nome="Kevin Lima"
+                            foto="/images/profilekevin.jpg"
+                            funcao="Desenvolvedor"
+                        />
+                        <CardSobreNos
+                            nome="Luiz Ricci"
+                            foto="/images/profileluiz.jpg"
+                            funcao="Desenvolvedor"
+                        />
+                        <CardSobreNos
+                            nome="Lucas Zani"
+                            foto="/images/profilezani.jpg"
+                            funcao="Desenvolvedor"
+                        />
+                    </div>
+                </div>
+
+                <div className={styles.contentJoin}>
+                    <h2 className={styles.titleJoin}>Junte-se a Nós!</h2>
+                    <p className={styles.descriptionJoin}>
+                        Se você ama festas e quer fazer parte de uma comunidade vibrante,
+                        junte-se ao PartyLife hoje mesmo! Estamos ansiosos para ver suas
+                        fotos e ouvir suas histórias de festa.
+                    </p>
+                </div>
+            </div>
+            <Footer />
         </div>
     );
 }
