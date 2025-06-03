3edf3d585a40733763aa14be67f1344950bf614c
yrkeviin <kevineziquiellima160@gmail.com>
2025-06-03 10:28:54 -0300

    Feat: Add bio in profile

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -46,6 +46,7 @@ export default function Profile() {
                     <div className={styles.profileInfo}>
                         <h1>Olá, Kevin</h1>
                         <h2>@username</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                     </div>
                 </div>
             </div>
